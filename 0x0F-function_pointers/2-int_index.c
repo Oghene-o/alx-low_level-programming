ff8cb9ce942d0e517f4f888bc3c3af2e89fd7c9d
Oghene-o <ogheneochuko1014@gmail.com>
2023-04-19 13:08:20 -0700

    update

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - ...
+ * @array: ...
+ * @size: ...
+ * @cmp: ...
+ *
+ * Return: ...
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+
+	while (i < size)
+	{
+		if (cmp(array[i]))
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
